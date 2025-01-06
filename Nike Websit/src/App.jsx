e7365ee412f226e3c5dc57d8327d6ab5490b4793
Khushtaunk28 <Khush.taunk@gmail.com>
2025-01-06 12:30:24 +0530

    created the nav bar and setup for the app

--- Nike Websit/src/App.jsx
+++ Nike Websit/src/App.jsx
@@ -1,7 +1,48 @@
+
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import SuperQulaity from "./sections/SuperQulaity";
+import  SpecialOffer from "./sections/SpecialOffer";
+import Footer from "./sections/Footer";
+import CustomerReviews from "./sections/CustomerReviews";
+import  Suscribe  from "./sections/Suscribe";
+import Nav from "./components/Nav";
+import Services from "./sections/Services";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline p-3">
-      Hello world!
-    </h1>
+   <main className="relative">
+    <Nav/>  
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>   
+      </section>
+
+    <section className="padding">
+     <PopularProducts/>
+    </section>
+
+    <section className="padding">
+      <SuperQulaity/>
+    </section>
+
+    <section className="padding">
+      <Services/>
+    </section>
+
+    <section className="padding-x py-10">
+      <SpecialOffer/>   </section>
+
+    <section className="bg-pale-blue padding">
+      <CustomerReviews/>
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Suscribe/>
+    </section>
+
+    <section className=" bg-black padding-x padding-t pb-8">
+      <Footer/>
+    </section>
+   </main>
   )
 }
---
+++ Nike Websit/src/sections/SuperQulaity.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuperQulaity = () => {
+  return (
+    <div>SuperQulaity</div>
+  )
+}
+
+export default SuperQulaity
