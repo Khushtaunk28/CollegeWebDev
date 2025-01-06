8f2c9ff6729596f9e05704be2d415024949ad91c
Khushtaunk28 <Khush.taunk@gmail.com>
2025-01-07 00:05:32 +0530

    created the service and service card

--- Nike Websit/src/sections/Services.jsx
+++ Nike Websit/src/sections/Services.jsx
@@ -1,8 +1,18 @@
+import { h1, section } from 'framer-motion/client'
 import React from 'react'
-
+import { services } from '../constants'
+import ServiceCard from '../components/ServiceCard'
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className='max-container flex flex-wrap gap-9 justify-center'>
+     {services.map((service) => {
+      return(
+        <ServiceCard key={service.label} 
+        {...service}/>
+      )
+
+     })}
+    </section>
   )
 }
 
