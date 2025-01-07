1c561b6df52cfe23c549da0fe862472b11ce5f5c
Khushtaunk28 <Khush.taunk@gmail.com>
2025-01-07 08:46:06 +0530

    created subscribe services

--- Nike Websit/src/sections/Subscribe.jsx
+++ Nike Websit/src/sections/Subscribe.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 
 const Suscribe = () => {
   return (
-    <section>
-      <div className='flex mt-2 '>
-        <h2 className='text-4xl font-bold font-palanquin'>Sign Up for <span className='text-coral-red'>Updates</span> & Newsletter
+    <section className='max-container flex justify-between items-center
+    max-lg:flex-col gap-10' id="contact-us">
+        <h2 className='text-4xl leading-[68px] lg:max-w-md font-bold font-palanquin'>Sign Up for <span className='text-coral-red'>Updates</span> & Newsletter
         </h2>
-    <div className='flex flex-col gap-3'>
-        <form action="sigup" className='gap-3 flex'>
-          <input type="text" placeholder="subscribe@nike.com" className='border-2 border-black p-2 rounded-full' />
+    <div className='lg:max-w-[40%] w-full flex items-center
+    max-sm:flex-col p-2.5 sm:border sm:border-slate-gray rounded-full
+    gap-5 border s'>
+        <form action="sigup" className='gap-3 flex '>
+          <input type="text" placeholder="subscribe@nike.com" className='input' />
           <Button label="Sign-Up"/>
         </form>
-        </div>
         </div>
     </section>
   )
